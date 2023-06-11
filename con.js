c695ec71b8ddc2bdd69220abea57cfaeeda10dac
Student <student@Souths-MacBook-Pro-6.local>
2023-06-11 12:58:15 -0500

    Cooper's Project

---
+++ con.js
@@ -0,0 +1,1 @@
+prompt("Who are you listening to?")
---
+++ js.js
@@ -0,0 +1,14 @@
+// alert("This is a alert")
+// prompt("this is a prompt")
+// prompt("this is a prompt")
+// prompt("this is a prompt")
+// prompt("this is a prompt")
+// prompt("this is a prompt")
+
+var chicken = 1;
+
+let mystring = "hello world"
+console.log = "Hello World"
+
+let isTrue = true;
+console.log(!isTrue)
